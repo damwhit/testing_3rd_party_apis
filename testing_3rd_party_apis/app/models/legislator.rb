92433cb8cc44412892a9b0a8540a28bf5ab41e7f
David Whitaker <damwhit@gmail.com>
2016-04-06 15:30:17 -0600

    refactored using instance methods

--- testing_3rd_party_apis/app/models/legislator.rb
+++ testing_3rd_party_apis/app/models/legislator.rb
@@ -1,4 +1,11 @@
 class Legislator < OpenStruct
+  attr_reader :first_name, :gender
+
+  def initialize(attrs)
+    @first_name = attrs[:first_name]
+    @gender = attrs[:gender]
+  end
+
   def self.find_by(params)
     service = SunlightService.new
     service.legislators(params).map { |raw_legislator|
