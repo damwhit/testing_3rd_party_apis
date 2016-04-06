18590bbbdee7d484551ca621f8220e959d5be608
David Whitaker <damwhit@gmail.com>
2016-04-06 10:19:34 -0600

    first test is written

---
+++ testing_3rd_party_apis/spec/services/sunlight_service_spec.rb
@@ -0,0 +1,13 @@
+describe SunlightService do
+  context "#legislators"
+  it "returns legislators" do
+    VCR.use_cassette("sunlight_service#legislators") do
+      service = SunlightService.new
+      legislators = service.legislators(gender: "F")
+      legislator = legislators.first
+      expect(legislator[:first_name]).to eq("Joni")
+      expect(legislator[:gender]).to eq("F")
+      expect(legislators.count).to eq(20)
+    end
+  end
+end
